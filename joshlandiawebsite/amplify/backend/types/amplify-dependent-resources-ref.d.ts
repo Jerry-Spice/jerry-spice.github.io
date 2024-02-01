5a4a4f55bbbde6dd900bb0143d9ff5b5166953d1
Jerry-Spice <joshlandiaofficial@gmail.com>
2024-02-01 01:07:27 -0500

    This should be a good start for my website

---
+++ joshlandiawebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "joshlandiawebsite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
